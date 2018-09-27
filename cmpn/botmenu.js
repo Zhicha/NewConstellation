08a4146738846add9ca2338335cb083e2b9f1663
Zhicha <37373092+Zhicha@users.noreply.github.com>
2018-09-27 23:26:27 +0200

    Add files via upload

---
+++ cmpn/botmenu.js
@@ -0,0 +1,2 @@
+document.write('Saint-Peterspurg Russia <span class="pochta"> link@newconstellation.ru</span> <a href="index.html">MAIN</a><a href="testzone.html">TZ</a><a href="romanslbr.php">LBR</a><a href="tree.html">TREE</a><a href="mantisbt-2.11.0/index.php">MNTS</a><a href="buh.php">BUH</a>\
+');
