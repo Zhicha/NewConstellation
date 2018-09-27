9d42ddc9ce8b12a3d1ac6149134f2672a60fc283
Zhicha <37373092+Zhicha@users.noreply.github.com>
2018-09-27 23:37:43 +0200

    Add files via upload

---
+++ tycoon/scripts/tycoon.js
@@ -0,0 +1,21 @@
+// My garage (Global)
+var garage01 = {men: 2, car: 1, microbus: 1; bus: 0, truck: 0, serviceStation: 1, serviceStationLevel: garage}
+var parking [miniBase, mediumBase, busesPark, universalTruckPark];
+var serviceStationLevel = [garage, box, bigBoxes, serviceStation, busesServiceStation, unicersalTrucksServiceStation];
+car1 {
+  fuel: gasoline, made: 1999, quality: 3, condition: 2, cost: "2000$", carRun: 200000, reliability: 4, Expenses100km: 8, service1km: 0.015
+};
+passMicrobus1 {
+  fuel: diesel, made: 2008, quality: 5, condition: 3, cost: "4000$", carRun: 200000, reliability: 4, Expenses100km: 10, service1km: 0.03
+}
+var park = [car1, passMicrobus1];
+var drivers =[];
+var serveceMan =[];
+var race001 = {
+  pointA: bigCity01,
+  pointB: oldCity01,
+  costRaceOneDay: 240,
+  distanceDay: 720,
+  costkm: costRaceOneDay / distanceDay
+}
+var myRace=[race001];
